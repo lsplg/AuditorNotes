abdd6d3fd7590eef9637020742f10f9462c9e230
lsplg <lvovaludmil@gmail.com>
2022-06-03 15:17:39 +0300

    delete unnecessary

--- src/main/java/Note.java
+++ src/main/java/Note.java
@@ -1,7 +1,22 @@
-public class Note {
-    private String note;
+import com.intellij.ProjectTopics;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 
-    public Note(String note) {
+import java.io.Serializable;
+
+public class Note implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String note;
+    private int line;
+    private String fileName;
+    private String projectName;
+
+    public Note(String note, int line, String fileName, String projectName) {
         this.note = note;
+        this.line = line;
+        this.fileName = fileName;
+        this.projectName = projectName;
     }
 }
